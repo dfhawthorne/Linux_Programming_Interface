711d42ceca768cd1fb15aa44f9c361556d294437
Douglas Hawthorne <dfhawthorne@computer.org>
2019-09-30 18:57:20 +1000

    Added README and removed acl library from realpath

---
+++ 18_Directories_and_Links/README.md
@@ -0,0 +1,8 @@
+# Chapter 18: Directories and Links
+
+## Exercise 18-3
+
+My solution is in `realpath.c`.
+
+1. Use `./make_realpath.sh` to compile and link this code.
+1. Use `./test_realpath.sh` to test this solution.
--- 18_Directories_and_Links/make_realpath.sh
+++ 18_Directories_and_Links/make_realpath.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 include_dirs="-I ../03_System_Programming_Concepts/"
 programs="realpath.c ../03_System_Programming_Concepts/error_functions.c"
-gcc -o realpath ${include_dirs} ${programs} -lacl
+gcc -o realpath ${include_dirs} ${programs}
