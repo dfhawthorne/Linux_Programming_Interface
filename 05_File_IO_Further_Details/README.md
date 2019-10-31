a4604c0b9b2a88f4ba9d242a831539190408e99a
Douglas Hawthorne <dfhawthorne@computer.org>
2019-10-31 18:28:48 +1100

    Solution to exercise 5-1

--- 05_File_IO_Further_Details/README.md
+++ 05_File_IO_Further_Details/README.md
@@ -8,3 +8,11 @@ To build and test this solution:
 1. Use `make large_file` to compile and link the code.
 1. Use `./large_file test.dat 10111222333` to test this code.
 1. Use `stat test.dat` to verify the size
+
+The expected response is similar to:
+```
+  File: test.dat
+  Size: 10111222337	Blocks: 8          IO Block: 4096   regular file
+Device: fd01h/64769d	Inode: 51643046    Links: 1
+Access: (0600/-rw-------)  Uid: ( 1000/ douglas)   Gid: ( 1000/ douglas)
+```
--- 05_File_IO_Further_Details/large_file.c
+++ 05_File_IO_Further_Details/large_file.c
@@ -14,7 +14,8 @@
 
    This program is Linux-specific.
 */
-#define _LARGEFILE64_SOURCE
+/* #define _LARGEFILE64_SOURCE */
+#define _FILE_OFFSET_BITS 64
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "tlpi_hdr.h"
@@ -23,17 +24,17 @@ int
 main(int argc, char *argv[])
 {
     int fd;
-    off64_t off;
+    off_t off;
 
     if (argc != 3 || strcmp(argv[1], "--help") == 0)
         usageErr("%s pathname offset\n", argv[0]);
 
-    fd = open64(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd == -1)
         errExit("open64");
 
     off = atoll(argv[2]);
-    if (lseek64(fd, off, SEEK_SET) == -1)
+    if (lseek(fd, off, SEEK_SET) == -1)
         errExit("lseek64");
 
     if (write(fd, "test", 4) == -1)
