60d958b278515af7a3134436097c946a642198dd
Douglas Hawthorne <dfhawthorne@computer.org>
2020-03-05 23:34:00 +1100

    Ex13-2: working solution

--- 13_File_I_O_Buffering/ex13_2.c
+++ 13_File_I_O_Buffering/ex13_2.c
@@ -26,55 +26,166 @@
    If compiled with -DUSE_FSYNC, perform an fsync() after each write, so that
    data and metadata are flushed to the disk.
 */
-#include <sys/stat.h>
+
 #include <fcntl.h>
-#include "tlpi_hdr.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+#include <time.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
-    size_t bufSize, numBytes, thisWrite, totWritten;
+    size_t buffer_size = 8192L,
+           num_bytes   = 8192L,
+           thisWrite   = 0L,
+           totWritten  = 0L;
     char *buf;
     int fd, openFlags;
+    int opt;
+    int verbosity = 0;
+    int open_sync = 0,
+        data_sync = 0,
+        file_sync = 0;
 
-    if (argc != 4 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s file num-bytes buf-size\n", argv[0]);
+    while ((opt = getopt(argc, argv, "b:dfhn:sv")) != -1) {
+        switch (opt) {
+        case 'b':
+            buffer_size = atoi(optarg);
+            break;
+        case 'd':
+            data_sync = 1;
+            break;
+        case 'f':
+            file_sync = 1;
+            break;
+        case 'h':
+            fprintf(stderr, "Usage: %s -h\n", argv[0]);
+            fprintf(stderr, "Usage: %s [-b buffer_size] [-d] [-f] [-n num_bytes] [-s] [-v] file-name\n",
+                    argv[0]);
+            exit(EXIT_FAILURE);
+        case 'n':
+            num_bytes = atoi(optarg);
+            break;
+        case 's':
+            open_sync = 1;
+            break;
+        case 'v':
+            verbosity++;
+            break;
+        default: /* '?' */
+            fprintf(stderr, "Usage: %s [-b buffer_size] [-s] [-v] input-file-name output-file-name\n",
+                    argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-    numBytes = getLong(argv[2], GN_GT_0, "num-bytes");
-    bufSize = getLong(argv[3], GN_GT_0, "buf-size");
+    if (verbosity > 0)
+    {
+        printf("buffer_size=%ld; verbosity=%d; data_sync=%c; file_sync=%c; open_sync=%c; num_bytes=%ld; argc=%d; optind=%d\n",
+           buffer_size, verbosity, (data_sync?'Y':'N'), (file_sync?'Y':'N'), (open_sync?'Y':'N'), num_bytes, argc, optind);
+    }
 
-    buf = malloc(bufSize);
+    if (optind >= argc) {
+        fprintf(stderr, "Expected one (1) argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (verbosity > 0)
+    {
+        printf("output file name argument = %s\n", argv[optind]);
+    }    
+
+    buf = malloc(buffer_size);
     if (buf == NULL)
-        errExit("malloc");
+    {
+        fprintf(stderr, "malloc: %m\n");
+        exit(EXIT_FAILURE);
+    }
 
     openFlags = O_CREAT | O_WRONLY;
 
-#if defined(USE_O_SYNC) && defined(O_SYNC)
-    openFlags |= O_SYNC;
-#endif
+    if (open_sync) openFlags |= O_SYNC;
 
-    fd = open(argv[1], openFlags, S_IRUSR | S_IWUSR);
+    fd = open(argv[optind], openFlags, S_IRUSR | S_IWUSR);
     if (fd == -1)
-        errExit("open");
+    {
+        fprintf(stderr, "open: %m\n");
+        exit(EXIT_FAILURE);
+    }
 
-    for (totWritten = 0; totWritten < numBytes;
+    for (totWritten = 0; totWritten < num_bytes;
             totWritten += thisWrite) {
-        thisWrite = min(bufSize, numBytes - totWritten);
+        thisWrite = (buffer_size < num_bytes - totWritten) ? buffer_size : num_bytes - totWritten;
 
         if (write(fd, buf, thisWrite) != thisWrite)
-            fatal("partial/failed write");
+        {
+            fprintf(stderr, "partial/failed write: %m\n");
+            exit(EXIT_FAILURE);
+        }
 
-#ifdef USE_FSYNC
-        if (fsync(fd))
-            errExit("fsync");
-#endif
-#ifdef USE_FDATASYNC
-        if (fdatasync(fd))
-            errExit("fdatasync");
-#endif
+        if (file_sync && fsync(fd))
+        {
+            fprintf(stderr, "fsync: %m\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (data_sync && fdatasync(fd))
+        {
+            fprintf(stderr, "fdatasync: %m\n");
+            exit(EXIT_FAILURE);
+        }
     }
 
     if (close(fd) == -1)
-        errExit("close");
+    {
+        fprintf(stderr, "close: %m");
+        exit(EXIT_FAILURE);
+    }
+    
+    /*------------------------------------------------------------------------*\
+    | Print statistics for later analysis                                      |
+    |  1. Block Size                                                           |
+    |  2. Open Synchronisation flag set?                                       |
+    |  3. File Synchronisation flag set?                                       |
+    |  4. Data Synchronisation flag set?                                       |
+    |  5. Number of bytes read                                                 |
+    |  6. Wall clock time in seconds                                           |
+    |  7. User time in seconds                                                 |
+    |  7. System time in seconds                                               |
+    \*------------------------------------------------------------------------*/
+    
+    struct tms t;
+    clock_t clockTime = clock();
+    long clockTicks = sysconf(_SC_CLK_TCK);  /* Clocks per tick for conversion */
+    
+    if (clockTicks == -1)
+    {
+        fprintf(stderr, "sysconf(_SC_CLK_TCK) failed: %m\n");
+        exit(EXIT_FAILURE);
+    }
+    if (clockTime  == -1)
+    {
+        fprintf(stderr, "clock() failed: %m\n");
+        exit(EXIT_FAILURE);
+    }
+    if (times(&t)  == -1)
+    {
+        fprintf(stderr, "times() failed: %m\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("%ld\t%c\t%c\t%c\t%ld\t%.2f\t%.2f\t%.2f\n",
+        buffer_size,
+        (open_sync) ? 's' : '-',
+        (file_sync) ? 'f' : '-',
+        (data_sync) ? 'd' : '-',
+        num_bytes,
+        (double) clockTime / CLOCKS_PER_SEC,
+        (double) t.tms_utime / clockTicks,
+        (double) t.tms_stime / clockTicks
+        );
     exit(EXIT_SUCCESS);
 }
