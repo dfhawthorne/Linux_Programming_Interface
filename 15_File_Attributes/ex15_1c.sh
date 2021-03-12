758bdd9de0c2ba7d0fe4958234efa382c908acbf
Douglas Hawthorne <dfhawthorne@computer.org>
2021-03-12 20:34:55 +1100

    Ex 15-1 (c): Move same to another dir

--- 15_File_Attributes/ex15_1c.sh
+++ 15_File_Attributes/ex15_1c.sh
@@ -34,6 +34,7 @@ do
         chmod "${file_perm}00" "${dir_name}"/old_file${file_perm}
         chmod "${file_perm}00" "${dir_name}"/samp_file${file_perm}
     done
+    chmod "${dir_perm}00" "${dir_name}"
 done
 
 # ------------------------------------------------------------------------------
@@ -45,6 +46,7 @@ printf "* [Exercise Results](#exercise-results)\n"
 printf "  * [File Creation](#file-creation)\n"
 printf "  * [File Access](#file-access)\n"
 printf "  * [Move File Between Directories](#move-file-between-directories)\n"
+printf "  * [Move Same File Between Directories](#move-same-file-between-directories)\n"
 
 # ------------------------------------------------------------------------------
 # Exercise Results
@@ -65,7 +67,6 @@ for dir_perm in {0..7}
 do
     printf "| %s------ " "${perm_str[$dir_perm]}"
     dir_name="test${dir_perm}"
-    chmod "${dir_perm}00" "${dir_name}"
     touch "${dir_name}"/new_file 2>/dev/null    && \
         printf "| %s " "${yes_mark}"            || \
         printf "| %s " "${no_mark}"
@@ -136,6 +137,48 @@ do
         do
             src_fn="${src_dir_name}/samp_file${file_perm}"
             dst_fn="test${dst_dir_perm}/new_file${file_perm}"
+            mv -f "${src_fn}" "${dst_fn}" 2>/dev/null             && \
+                mv "${dst_fn}" "${src_fn}"                        && \
+                printf "| %s " "${yes_mark}"                      || \
+                printf "| %s " "${no_mark}"
+        done
+        printf "|\n"
+    done
+done
+
+
+# ------------------------------------------------------------------------------
+# Move Same File Between Directories
+# ------------------------------------------------------------------------------
+
+printf "\n## Move Same File Between Directories\n\n"
+
+printf "| Src Dir Perm | File Perm |"
+for dst_dir_perm in {0..7}
+do
+    printf " %s------ |" "${perm_str[${dst_dir_perm}]}"
+done
+printf "\n"
+printf "| ------------ | --------- |"
+for dst_dir_perm in {0..7}
+do
+    printf " --------- |"
+done
+printf "\n"
+
+for src_dir_perm in {0..7}
+do
+    src_dir_name="test${src_dir_perm}"
+    for file_perm in {0..7}
+    do
+        [[ ${file_perm} == 0 ]]                               && \
+            printf "| %s------ " "${perm_str[$src_dir_perm]}" || \
+            printf "| "
+        printf "| %s------ " "${perm_str[$file_perm]}"
+        for dst_dir_perm in {0..7}
+        do
+            src_fn="${src_dir_name}/samp_file${file_perm}"
+            dst_fn="test${dst_dir_perm}/samp_file${file_perm}"
             mv -f "${src_fn}" "${dst_fn}" 2>/dev/null             && \
                 mv "${dst_fn}" "${src_fn}"                        && \
                 printf "| %s " "${yes_mark}"                      || \
