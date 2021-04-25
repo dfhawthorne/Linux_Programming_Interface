4e229d7fb3c7452a8d43ffa9a648d43d54c88fda
Douglas Hawthorne <dfhawthorne@computer.org>
2021-04-25 10:16:04 +1000

    Solutions to 15-4 and 15-5

--- 15_File_Attributes/README.md
+++ 15_File_Attributes/README.md
@@ -1,5 +1,83 @@
-Chapter 15: File atAttributes
-===========================
+# Chapter 15: File Attributes
 
-Exercise 15-1
--------------
+## Table of Contents
+
+  * [Exercise 15-1](#exercise-15-1)
+    * [Exercise 15-1 (a)](#exercise-15-1-a)
+    * [Exercise 15-1 (b)](#exercise-15-1-b)
+    * [Exercise 15-1 (c)](#exercise-15-1-c)
+  * [Exercise 15-2](#exercise-15-2)
+  * [Exercise 15-3](#exercise-15-3)
+  * [Exercise 15-4](#exercise-15-4)
+  * [Exercise 15-5](#exercise-15-5)
+
+
+## Exercise 15-1
+
+### Exercise 15-1 (a)
+
+My answer is in [Ex15_1A.md](Ex15_1A.md)
+
+### Exercise 15-1 (b)
+
+My answer is in [Ex15_1B.md](Ex15_1B.md)
+
+### Exercise 15-1 (c)
+
+1. Run `make my_access`
+1. Run `./ex15_1c.sh >results.md`
+1. Results are in [Ex15_1C.md](Ex15_1C.md)
+
+## Exercise 15-2
+
+My answer is in [Ex15_2.md](Ex15_2.md)
+
+## Exercise 15-3
+
+1. Run `make t_stat`
+1. Run `touch test`
+1. Run `./t_stat test`
+
+Sample output is:
+```
+File type:                regular file
+Device containing i-node: major=253   minor=1
+I-node number:            51773935
+Mode:                     100400 (r--------)
+Number of (hard) links:   1
+Ownership:                UID=1000   GID=1000
+File size:                0 bytes
+Optimal I/O block size:   4096 bytes
+512B blocks allocated:    0
+Last file access:         Thu Apr  1 08:26:36.752583228 2021
+Last file modification:   Sun Mar 21 21:42:26.305140934 2021
+Last status change:       Thu Apr  1 21:23:46.819378661 2021
+```
+
+## Exercise 15-4
+
+1. Run `make my_access`
+1. Run `sudo chown test_user my_access`
+1. Run `sudo chmod u+sx my_access`
+1. Run `touch test`
+1. Run `chmod 400 test`
+1. Run `./my_access -f test`
+1. Run `./my_access -r test`
+1. Run `./my_access -w test`
+
+Sample output is:
+```
+$ ./my_access -f test
+Access for uid to 'test' using mask 0 is 0 (Success)
+seteuid(32767) failed: Operation not permitted
+$ ./my_access -r test
+Access for uid to 'test' using mask 4 is 0 (Success)
+seteuid(32766) failed: Operation not permitted
+$ ./my_access -w test
+Access for uid to 'test' using mask 2 is -1 (Permission denied)
+seteuid(32764) failed: Operation not permitted
+```
+
+## Exercise 15-5
+
+My answer is in [EX_15_5.md](EX_15_5.md)
--- 15_File_Attributes/my_access.c
+++ 15_File_Attributes/my_access.c
@@ -15,9 +15,12 @@
  * non-root users, the check uses an empty set of capabilities.
  ******************************************************************************/
 
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 
@@ -26,23 +29,49 @@ void print_help(char *msg, int rc)
     if (msg != NULL) fprintf(stderr, "%s\n", msg);
     fprintf(stderr, "my_access -h\n");
     fprintf(stderr, "my_access -f file\n");
-    fprintf(stderr, "my_access [-r] [-w] [-x] file\n");
+    fprintf(stderr, "my_access [-r] [-w] [-x] [-u uid] file\n");
     fprintf(stderr, "\t-h\tPrint this help message\n");
     fprintf(stderr, "\t-f\tTest for existence of file\n");
     fprintf(stderr, "\t-r\tTest whether effective user can read file\n");
     fprintf(stderr, "\t-w\tTest whether effective user can write file\n");
     fprintf(stderr, "\t-x\tTest whether effective user can execute file\n");
+    fprintf(stderr, "\n\t-u\tEffective User ID to use\n");
+    
     exit(rc);
 }
 
-int my_access(const char *pathname, int mode)
+int euid_access(const char *pathname, int mode, uid_t uid)
 {
+    int save_errno = 0;
+    int res = 0;
+    
+    struct stat statbuf;
+    
+    res = access(pathname, mode);
+    
+    if (res == 0) return 0;
+    
+    save_errno = errno;
+    
+    if (stat(pathname, &statbuf) == -1) return -1;
+    
+    if (mode == F_OK) return 0; /* File exists */
+ 
+    if ((uid == statbuf.st_uid) &&
+        ((statbuf.st_mode & S_IRWXU) == mode << 8))
+    {
+        return 0; /* owner has required permissions */
+    }
+
+    errno = EACCES;
+    return -1;
 }
 
 int
 main(int argc, char *argv[])
 {
     int fname;                  /* Location of filename argument in argv[] */
+    uid_t euid;                 /* effective user-id */
     int opt;
     int f_mode = 0;             /* F_OK maps to zero (0) */
     int rwx_mode = 0;
@@ -50,9 +79,11 @@ main(int argc, char *argv[])
     char *invalid_opt_msg;
     char *file_missing_msg = "file name is missing";
     char *conflict_args_msg = "specify either -f or a combination of -r -w -x";
+    char *uid_msg = "Access for uid to '%s' using mask %d is %d (%m)\n";
+    char *euid_msg = "Access for euid to '%s' using mask %d is %d (%m)\n";
     
     invalid_opt_msg = malloc(strlen(invalid_opt_msg_fmt));
-    while ((opt = getopt(argc, argv, ":hfrwx")) != -1)
+    while ((opt = getopt(argc, argv, ":hfru:wx")) != -1)
     {
         switch (opt)
         {
@@ -61,6 +92,7 @@ main(int argc, char *argv[])
             case 'r': rwx_mode |= R_OK; break;
             case 'w': rwx_mode |= W_OK; break;
             case 'x': rwx_mode |= X_OK; break;
+            case 'u': euid = atoi(optarg); break;
             case '?':
                 sprintf(invalid_opt_msg, invalid_opt_msg_fmt, (char)optopt);
                 print_help(invalid_opt_msg, EXIT_FAILURE);
@@ -80,5 +112,20 @@ main(int argc, char *argv[])
         if (rwx_mode != 0) print_help(conflict_args_msg, EXIT_FAILURE);
     }
 
+    int mode = (f_mode) ? F_OK : rwx_mode;
+    int res = 0;
+
+    res = access(argv[fname], mode);
+    printf(uid_msg, argv[fname], mode, res);
+
+    if (seteuid(euid) != 0)
+    {
+        fprintf(stderr, "seteuid(%d) failed: %m\n", euid);
+        exit(EXIT_FAILURE);
+    }
+
+    res = euid_access(argv[fname], mode, euid);
+    printf(euid_msg, argv[fname], mode, res);
+    
     exit(EXIT_SUCCESS);
 }
